525ed19469471f7c7690803ef2ef840e77a6bf51
Cro0ky <143035135+Cro0ky@users.noreply.github.com>
2024-01-23 11:32:47 +0300

    Add files via upload

---
+++ JScourse/main.js
@@ -0,0 +1,133 @@
+//1
+// const name = prompt("What's your name?")
+// alert(`ur name is ${name}`)
+
+//2
+// let a = 1, b = 1;
+
+// let c = ++a
+// let d = b++
+
+// console.log(c,d);
+//c = 2, d = 1
+
+//3
+// let x = 1 + (a *= 2)
+//a = 5
+// console.log(x);
+
+//4
+// console.log("" + 1 + 0); // 10
+// console.log("" - 1 + 0); // -1
+// console.log(true + false); // 1
+// console.log(6 / '3'); // 2
+// console.log('2' * '3'); // 6
+// console.log(4 + 5 + 'px'); // 9px
+// console.log('$' + 4 + 5); // $45
+// console.log('4' - 2); //2
+// console.log('4px' - 2); //Nan
+// console.log('   -9  ' + 5); // -9 5
+// console.log('   -9  ' - 5); // -14
+// console.log(null + 1); // 1 
+// console.log(undefined + 1); // NaN
+// console.log(' /t /n' - 2); // NaN
+
+//5
+// let a = Number(prompt('first num ', 1))
+// let b = Number(prompt('second num ', 2))
+
+// alert(a + b)
+
+//6
+// let  num = 3
+// alert(num)
+
+//7
+// let a = 10, b = 2
+// alert(a + b, a-b, a*b, a/ b)
+// alert(a - b)
+// alert(a * b)
+// alert(a / b)
+
+//8 
+// let c = 15, d = 2
+// let result = c + d
+// alert(result)
+
+//9
+// let a = 10, b = 2, c=5;
+// alert(a+b+c)
+
+//10
+// let a = 17, b = 10
+// let c = a - b
+// let d = 7
+// let result = c + d 
+// alert(result)// 14
+
+//11
+// let str = 'Привет, мир!'
+// alert(str)
+
+//12
+// let str1 = 'Hello, ', str2 = 'world!'
+// alert(str1 + str2)
+
+//13
+// let name = prompt('name: ')
+// alert(`ur name: ${name}`)
+
+//14
+// let age = prompt('age: ')
+// alert(`i am ${age} years old`)
+
+//15
+// let name = prompt('what is your name? ')
+// alert(`ur name: ${name}`)
+
+//16
+// let num = prompt('num: ')
+// alert(`${num ** 2}`)
+
+//17
+// let str = 'abcde'
+// alert(str[0])
+// alert(str[2])
+// alert(str[4])
+
+//18
+// let sec = 1
+// let hour = sec * 60
+// let day = hour * 24
+// let month = day * 31
+// alert(`seconds in hour = ${hour}`)
+// alert(`seconds in day = ${day}`)
+// alert(`seconds in month = ${month}`)
+
+//19
+// let hour = prompt('hour: ')
+// let minute = prompt('minutes: ')
+// let seconds = prompt('seconds: ')
+// alert(`${hour}:${minute}:${seconds}`)
+
+//20
+// let num = prompt('num: ')
+// alert(num ** 2)
+
+//21
+// let num = 47
+// num += 7
+// num -= 18
+// num *= 10
+// num /= 15
+// alert(num)
+
+//22
+// let num = 10
+// ++num
+// ++num
+// --num
+// alert(num)
+
+
+
